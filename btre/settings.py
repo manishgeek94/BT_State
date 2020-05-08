ccb11591f461e8fc9c72666fd0be6222e7adf45c
Manish <manishk19365@gmail.com>
2020-05-08 22:48:27 +0530

    DashBoard Customization

--- btre/settings.py
+++ btre/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
 ]
 
 MIDDLEWARE = [
@@ -130,3 +131,7 @@ STATICFILES_DIRS = [
     
     os.path.join(BASE_DIR,'btre/static')
 ]
+
+#Media folder settings
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_URL = '/media/'
