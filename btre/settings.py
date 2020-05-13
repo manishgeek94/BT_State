ac393a3be5aa115b16fb16ecc6a47880fa680e23
Manish <manishk19365@gmail.com>
2020-05-14 03:14:46 +0530

    email setting change

--- btre/settings.py
+++ btre/settings.py
@@ -147,12 +147,6 @@ MESSAGE_TAGS = {
     
 }
 
-#Email Config
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'cutemanish786@gmail.com'
-EMAIL_HOST_PASSWORD = ''
-EMAIL_USE_TLS = True
 
 try:
     from .local_settings import *
