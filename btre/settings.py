ad1c34556328caf0975c32dc4f1b3f99f1640960
Manish <manishk19365@gmail.com>
2020-05-14 03:06:28 +0530

    local settings changed

--- btre/settings.py
+++ btre/settings.py
@@ -151,5 +151,10 @@ MESSAGE_TAGS = {
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'cutemanish786@gmail.com'
-EMAIL_HOST_PASSWORD = 'Manraj9449  '
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
